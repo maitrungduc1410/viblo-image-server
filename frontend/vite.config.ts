e40bcab79c026db697dc610496f9406a0e4d9c69
Mai Trung Duc <maitrungduc1410@gmail.com>
2024-05-02 23:29:34 +0800

    fix: local setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,22 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 const createProxyConfig = (width: number) => ({
-  target: "http://host.docker.internal:8081",
+  target: "http://imgproxy:8080",
   changeOrigin: true,
   rewrite: (path: string) => {
     const filename = path.split("/").pop();
-    return `/insecure/resize:fit:${width}:0:no:0/plain/http://host.docker.internal:3000/images/${filename}`;
+    return `/insecure/resize:fit:${width}:0:no:0/plain/http://backend:3000/images/${filename}`;
   },
 });
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://host.docker.internal:3000",
+      "/api": "http://backend:3000",
       "/images/full": {
-        target: "http://host.docker.internal:3000",
+        target: "http://backend:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/images\/full/, "/images"),
       },
